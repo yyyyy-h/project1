e51b573c5214a4a1d2aeccad3ab122751b3fc862
yyyyy-h <1597336911@qq.com>
2026-03-18 11:26:35 +0800

    feat: 移除阿里云AK/SK，配置环境变量

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,47 @@
+package com.sky.service.impl;
+
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        //菜品表插入数据
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        dishMapper.insert(dish);
+        //获取插入的菜品的id
+        Long dishId = dish.getId();
+        //口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        for (DishFlavor flavor : flavors) {
+            flavor.setDishId(dishId);
+        }
+        if (flavors != null && flavors.size() > 0) {
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
